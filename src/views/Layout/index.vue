0ae81d5161e8591ba3bb6407e0d3340422edea75
taoran <taoran376@gmail.com>
2024-05-24 22:25:00 +0800

    pinia优化导航数据

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -5,6 +5,18 @@ import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
 import LayoutFixed from './components/LayoutFixed.vue'
 
+
+//触发获取导航列表的action
+
+import {useCategoryStore} from '@/stores/category'
+import { onMounted } from 'vue'
+
+const categoryStore = useCategoryStore() //获取实例对象
+// console.log(categoryStore);
+
+onMounted(()=>{
+  categoryStore.getCategory()
+})
 </script>
 
 <template>
